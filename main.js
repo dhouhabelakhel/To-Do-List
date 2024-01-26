1a0a045f846b686f3418b31b328ed407438fa810
dhouha belakhel <dhouhabelakhel2001@gmail.com>
2024-01-26 20:12:39 +0100

    ajout du model 3d de book

--- main.js
+++ main.js
@@ -1,1 +1,66 @@
-import './style.css'
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+// To allow for the camera to move around the scene
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+
+const canvas = document.querySelector('canvas.webgl');
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+camera.position.z = 1;
+scene.add(camera);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(directionalLight);
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+let model;
+const gltfLoader = new GLTFLoader();
+gltfLoader.load(
+  '/paladins_book/scene.gltf',
+  (gltf) => {
+    model = gltf.scene;
+    scene.add(model);
+    console.log('Model loaded successfully');
+  
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+  },
+  (error) => {
+    console.error('An error occurred while loading the GLTF model:', error);
+  }
+);
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+ 
+   const etat= renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
